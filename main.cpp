4d18f268639d42a0388d85dfd43b714fe6c90aad
sohiearth <sohiearth@gmail.com>
2024-05-09 22:53:08 +0900

    Create class Application and fix TTF Error

--- main.cpp
+++ main.cpp
@@ -7,14 +7,17 @@
 bool running = false;
 
 int main(int argc, char* argv[]) {
-    if ((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) | TTF_Init()) != 0) {
-    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-    return 1;
+    if ((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+    if (TTF_Init() != -1) {
+        SDL_Log("Unable to initialize TTF: %s", TTF_GetError());
     }
     
     // Create a window for main loop
     // [ - IMPORTANT CHANGE - ] Change name for engine by reading from a resource file
-    SDL_Window* window = SDL_CreateWindow("Vortex Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Vortex Engine", 0, 0, 800, 600, SDL_WINDOW_FULLSCREEN_DESKTOP);
     if (!window) {
         SDL_Log("Unable to create window: %s", SDL_GetError());
         SDL_Quit();
@@ -46,6 +49,12 @@ int main(int argc, char* argv[]) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
+    
     return 0;
 }
+
+class Application {
+    void Quit() {
+        running = false;
+    }
+};
